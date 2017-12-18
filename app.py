d3b65b442bd01f7210a5242c3be57accfd6d9551
aengelen <aengelen9@gmail.com>
2017-12-18 12:29:57 -0800

    Wit test

--- app.py
+++ app.py
@@ -29,19 +29,32 @@ def sparkhook():
 
         if str(jsonAnswer['data']['personEmail']) != str(botDetails.emails[0]): # If the message is not sent by the bot
 
-            botName = str(botDetails.displayName)
-            botFirstName = botName.split(None, 1)[0]
+            botName = str(botDetails.displayName) # Get bot's display name
+            botFirstName = botName.split(None, 1)[0] # Get bot's "first name"
 
-            sparkMessage = str(api.messages.get(jsonAnswer['data']['id']))
+            sparkMessage = str(api.messages.get(jsonAnswer['data']['id'])) # Get message text from message ID
             sparkMessage = sparkMessage.split(botFirstName,1)[1] #Remove bot's first name from message
 
-            #witClient = Wit(access_token="NXZNQT2BEKTEYCT2NHATEDVIKB3HAZTU")
-            #witResp = witClient.message(sparkMessage)
+            witClient = Wit(access_token=WIT_TOKEN) # Create Wit session
+            witResp = witClient.message(sparkMessage) # Answer from Wit after sending message in Spark
 
-            textAnswer = 'Hello <@personEmail:' + str(jsonAnswer['data']['personEmail']) + '>'
+            if data['entities'].get('email'):
+                emailAddress = str(data['entities']['email'][0]['value'])
+                botAnswer = api.messages.create(roomId=SPACE_ID, text=str(emailAddress))
+
+            if data['entities'].get('add_user_intent'):
+                addUserConf = str(data['entities']['add_user_intent'][0]['confidence'])
+
+            if data['entities'].get('remove_user_intent'):
+                removeUserConf = str(data['entities']['remove_user_intent'][0]['confidence'])
+
+            if data['entities'].get('greetings'):
+                greetingsConf = str(data['entities']['greetings'][0]['confidence'])
+
+            #textAnswer = 'Hello <@personEmail:' + str(jsonAnswer['data']['personEmail']) + '>'
 
             #botAnswer = api.messages.create(roomId=SPACE_ID, text=str(witResp))
-            botAnswer2 = api.messages.create(roomId=SPACE_ID, markdown=textAnswer)
+            #botAnswer2 = api.messages.create(roomId=SPACE_ID, markdown=textAnswer)
 
 if __name__ == '__main__':
     app.run()
