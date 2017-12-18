e6671e035b60e69ffc35c193859b85db8e65dba9
aengelen <aengelen9@gmail.com>
2017-12-18 05:37:44 -0800

    Bot answers message

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 from ciscosparkapi import CiscoSparkAPI
+import json
 
 app = Flask(__name__)
 
 
 BOT_TOKEN = 'ZjBhYWVlYWUtYTY4NC00MTFiLThhOGItODEzOGYzZDYyNzJiYzJjNGVhNjAtZWM2'
+SPACE_ID = 'Y2lzY29zcGFyazovL3VzL1JPT00vYWQ3ODExMDAtZTNmMS0xMWU3LTllYTUtNjFmNzA3MjJlYjkz'
 
 api = CiscoSparkAPI(access_token=BOT_TOKEN)
 
@@ -19,7 +21,19 @@ def sparkhook():
 
     if request.method == 'POST':
 
-        jsonAnswer = json.loads(request.data)
+        jsonAnswer = json.loads(request.data) # Format data from POST into JSON
+
+        botDetails = api.people.me() # Get details of the bot from its token
+
+        if str(jsonAnswer["data"]["personEmail"]) != str(botDetails.emails[0]): # If the message is not sent by the bot
+
+            botName = str(botDetails.displayName)
+            botFirstName = botName.split(None, 1)[0]
+
+            sparkMessage = str(api.messages.get(jsonAnswer["data"]["id"]))
+            sparkMessage = sparkMessage.split(botFirstName,1)[1] #Remove bot's first name from message
+
+            botAnswer = api.messages.create(roomId=SPACE_ID, text='Hello Sir')
 
 if __name__ == '__main__':
     app.run()
