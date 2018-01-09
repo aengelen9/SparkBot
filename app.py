543e7a9fcd3a4c178626dca71513ca01cf678016
aengelen <aengelen9@gmail.com>
2018-01-09 05:08:01 -0800

    Handling Files

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 BOT_TOKEN = 'OTEwNTA4MGMtYTQ0OS00NjRlLWFlMTgtYWM4YjMxN2Q5NTcyOWE3MWM5YjgtODU5'
 SPACE_ID = 'Y2lzY29zcGFyazovL3VzL1JPT00vYTJhZWI5NzAtZTRiNi0xMWU3LWI2MDQtZmRmZDEyNDAyM2E0'
-WIT_TOKEN = 'NXZNQT2BEKTEYCT2NHATEDVIKB3HAZTU'
+#WIT_TOKEN = 'NXZNQT2BEKTEYCT2NHATEDVIKB3HAZTU'
 
 api = CiscoSparkAPI(access_token=BOT_TOKEN)
 
@@ -33,52 +33,14 @@ def sparkhook():
             botFirstName = botName.split(None, 1)[0] # Get bot's "first name"
 
             sparkMessage = api.messages.get(jsonAnswer['data']['id']) # Get message object text from message ID
-            sparkMessage = str(sparkMessage.text) # Get message text
-            sparkMessage = sparkMessage.split(botFirstName,1)[1] #Remove bot's first name from message
+            sparkMsgText = str(sparkMessage.text) # Get message text
+            sparkMsgText = sparkMsgText.split(botFirstName,1)[1] #Remove bot's first name from message
+            sparkMsgFiles = str(sparkMessage.files)
 
-            witClient = Wit(access_token=WIT_TOKEN) # Create Wit session
-            witResp = witClient.message(sparkMessage) # Answer from Wit after sending message in Spark
-
-            if witResp['entities'].get('greetings'): # If the entity "greetings" exist
-                greetingsConf = float(witResp['entities']['greetings'][0]['confidence'])
-                if greetingsConf > 0.85: # If we are confident that the user is greeting
-                    flagHello = 1
-                else:
-                    flagHello = 0
-            else:
-                flagHello = 0
-
-            if witResp['entities'].get('add_user_intent'):
-                addUserConf = float(witResp['entities']['add_user_intent'][0]['confidence'])
-                if addUserConf > 0.85:
-                    flagAdd = 1
-                else:
-                    flagAdd = 0
-            else:
-                flagAdd = 0
-
-            if witResp['entities'].get('email'):
-                emailConf = float(witResp['entities']['email'][0]['confidence'])
-                if emailConf > 0.85:
-                    emailAddress = str(witResp['entities']['email'][0]['value'])
-                    flagEmail = 1
-                else:
-                    flagEmail = 0
-            else:
-                flagEmail = 0
-
-
-            if witResp['entities'].get('remove_user_intent'):
-                removeUserConf = float(witResp['entities']['remove_user_intent'][0]['confidence'])
-                if removeUserConf > 0.85:
-                    flagRemove = 1
-                else:
-                    flagRemove = 0
-            else:
-                flagRemove = 0
-
+            botAnswered = api.messages.create(roomId=SPACE_ID, text=sparkMsgFiles)
 
             # Answering logic
+            '''
             if (flagAdd == 1) and (flagEmail == 1):
                 participantAdded = api.memberships.create(roomId=SPACE_ID, personEmail=str(emailAddress), isModerator=False)
                 textAnswer = 'I have added <@personEmail:' + str(emailAddress) + '> to the space.'
@@ -103,7 +65,7 @@ def sparkhook():
             else:
                 textAnswer = 'I am sorry but I am not sure that I understand. I am really not that smart. I can only **add** or **remove** a participant from this space.'
                 botAnswered = api.messages.create(roomId=SPACE_ID, markdown=textAnswer)
-
+            '''
 
     return 'OK'
 
