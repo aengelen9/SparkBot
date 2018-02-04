2adb303a4e5da51051493b4dbb0165a0c06aba59
aengelen <aengelen9@gmail.com>
2018-02-04 14:38:00 -0800

    Pod assignment

--- app_backup.py
+++ app_backup.py
@@ -7,9 +7,7 @@ import csv
 app = Flask(__name__)
 
 
-#BOT_TOKEN = 'OTc5OGY3YjEtM2I4OC00ZjhiLTg5YjMtYzU1NTdkOGM4NTViMTg0ZDYxYzMtOTRm'
 BOT_TOKEN = 'OTc5OGY3YjEtM2I4OC00ZjhiLTg5YjMtYzU1NTdkOGM4NTViMTg0ZDYxYzMtOTRm'
-MY_TOKEN = 'Zjk5ODUzN2QtN2FlOS00ODQ0LWI0NTgtOWQ3MjY5MmU5ZmQ0NGZhNDY4ZTEtNTli'
 SPACE_ID = 'Y2lzY29zcGFyazovL3VzL1JPT00vZjUwNjZjZTAtZjYxMy0xMWU3LTkyYTgtYjNiNGFhZDUxNzIy'
 
 api = CiscoSparkAPI(access_token=BOT_TOKEN)
@@ -40,7 +38,7 @@ def sparkhook():
 
             # Say hello if the message doesn't contain a file
             if not sparkMessage.files:
-                textAnswer = 'Hello <@personEmail:' + str(jsonAnswer['data']['personEmail']) + '>, you can send me a CSV file including a list of e-mail addresses and I will assign them a pod.'
+                textAnswer = 'Hello <@personEmail:' + str(jsonAnswer['data']['personEmail']) + '>, you can send me a CSV file including a list of e-mail addresses and I will add them to this space.'
                 botAnswered = api.messages.create(roomId=SPACE_ID, markdown=textAnswer)
 
             # If the message comes with a file
@@ -56,12 +54,12 @@ def sparkhook():
                     # If the file extension is CSV
                     if str(getResponse.headers['Content-Type']) == 'text/csv':
                         decodedContent = getResponse.content.decode('utf-8')
-                        csvFile = csv.reader(decodedContent.splitlines(), delimiter=',')
+                        csvFile = csv.reader(decodedContent.splitlines(), delimiter=';')
                         listEmails = list(csvFile)
                         for row in listEmails: # Creating one list for each line in the file
                             if i != 0:
-                                #participantAdded = api.memberships.create(roomId=SPACE_ID, personEmail=str(row[5]), isModerator=False) # Add participant from e-mail field
-                                botAnswered = api.messages.create(roomId=SPACE_ID, text=str(row[5]))
+                                participantAdded = api.memberships.create(roomId=SPACE_ID, personEmail=str(row[2]), isModerator=False) # Add participant from e-mail field
+                                botAnswered = api.messages.create(roomId=SPACE_ID, text=str(api.exceptions.SparkApiError))
                             i += 1
 
                     # If the attached file is not a CSV
