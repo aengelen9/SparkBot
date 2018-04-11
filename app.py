8305a0bce792086122adfac5f4a3eaf627789e51
aengelen <aengelen9@gmail.com>
2018-04-11 02:18:02 -0700

    MAC Address REGEX

--- app.py
+++ app.py
@@ -121,7 +121,8 @@ def sparkhook():
                         imgText = postGoogleOCR(encodedImgUtf8)
                         imgText = imgText['responses'][0]['textAnnotations'][0]['description']
 
-                        p = re.compile('([0-9A-F]{2}[:-]){5}([0-9A-F]{2})|([0-9A-F]{12})', re.IGNORECASE)
+                        #p = re.compile('([0-9A-F]{2}[:-]){5}([0-9A-F]{2})|([0-9A-F]{12})', re.IGNORECASE)
+                        p = re.compile('([0-9A-F]{2}[:-]?){5}([0-9A-F]{2})', re.IGNORECASE)
 
                         macAddr = re.findall(p, imgText)
                         #macAddr = macAddr[0] if macAddr else ''
@@ -139,7 +140,7 @@ def sparkhook():
 
 
                        
-                        botAnswered = api.messages.create(roomId=SPACE_ID, text=str(imgText))
+                        botAnswered = api.messages.create(roomId=SPACE_ID, text=str(macAddr))
                                 
 
 
