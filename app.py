591aaf666fd4ab501a02b31530c8564b626f7d66
aengelen <aengelen9@gmail.com>
2018-06-28 08:40:17 -0700

    adding assurance apis

--- app.py
+++ app.py
@@ -189,11 +189,12 @@ def sparkhook():
                             clientTime = dnac.getClientTime(cookie)
                             #botAnswered = api.messages.create(roomId=SPACE_ID, markdown=str(clientTime))
                             clientHealth = dnac.getClientHealth(cookie, macAddr, clientTime)
-                            botAnswered = api.messages.create(roomId=SPACE_ID, markdown=str(clientHealth['location']))
+                            #botAnswered = api.messages.create(roomId=SPACE_ID, markdown=str(clientHealth['location']))
 
                             answerString = 'Searching for host MAC address **' + macAddr + '**\n\n- Host IP: ' + hostIp
                             answerString = answerString + '\n- Host Type: ' + hostType
                             answerString = answerString + '\n- Host Subtype: ' + hostSubType
+                            answerString = answerString + '\n- Host Health Score: ' + clientHealth['healthScore'][0]['score']
                             answerString = answerString + '\n- Connected Network Device: ' + deviceType
                             answerString = answerString + '\n- Connected Network Device IP: ' + connectedNetworkDeviceIpAddress
                             answerString = answerString + '\n- Interface: ' + connectedInterfaceName
